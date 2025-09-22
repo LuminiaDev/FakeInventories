47229ebe5da5ec1060cf72fe2832581c4244897b
MEFRREEX <mefrreex@gmail.com>
2025-09-23 02:14:33 +0400

    feat: publishing on repo.luminiadev.com

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,26 +7,43 @@ group = "me.iwareq.fakeinventories"
 version = "1.1.8"
 
 repositories {
-    mavenLocal()
     mavenCentral()
-    maven("https://repo.opencollab.dev/maven-releases")
-    maven("https://repo.opencollab.dev/maven-snapshots")
+    maven("https://repo.lanink.cn/repository/maven-public/")
 }
 
 dependencies {
-    implementation("cn.nukkit:nukkit:1.0-SNAPSHOT")
-
-    compileOnly("org.projectlombok:lombok:1.18.30")
-    annotationProcessor("org.projectlombok:lombok:1.18.30")
+    compileOnly("cn.nukkit:Nukkit:MOT-SNAPSHOT")
+    compileOnly("org.projectlombok:lombok:1.18.38")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
 }
 
 tasks.withType<JavaCompile> {
-    options.encoding = Charsets.UTF_8.name()
+    options.encoding = "UTF-8"
 }
 
 tasks.withType<ProcessResources> {
-    filteringCharset = Charsets.UTF_8.name()
     filesMatching("plugin.yml") {
-        expand("version" to version)
+        expand(project.properties)
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name.lowercase()
+            version = project.version.toString()
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "luminiadev"
+            url = uri("https://repo.luminiadev.com/releases")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
     }
 }
